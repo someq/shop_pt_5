610939f2035bca9f3535636491cf4e18413c6033
Farid <thesomeq@gmail.com>
2020-07-30 20:13:27 +0600

    Добавлена модель товара, создана миграция, и модель зарегистрирована в админке.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+DEFAULT_CATEGORY = 'other'
+CATEGORY_CHOICES = (
+    (DEFAULT_CATEGORY, 'Разное'),
+    ('food', 'Продукты питания'),
+    ('household', 'Хоз. товары'),
+    ('toys', 'Детские игрушки'),
+    ('appliances', 'Бытовая Техника')
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    category = models.CharField(max_length=20, verbose_name='Категория',
+                                choices=CATEGORY_CHOICES, default=DEFAULT_CATEGORY)
+    amount = models.IntegerField(verbose_name='Остаток')
+    price = models.DecimalField(verbose_name='Цена', max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.name} - {self.amount}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
